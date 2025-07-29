afb9107520e0abe43a27b135c8f9eadbe76c9f03
BagasBikang <bagasadit78@gmail.com>
2025-07-29 17:53:24 +0700

    file commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,225 @@
+"use client";
+
+import React from "react";
 import Image from "next/image";
+import { useEffect, useState} from "react";
+import { Linkedin, Instagram, Github } from "lucide-react";
+import { FaHtml5, FaCss3Alt, FaJs, FaPhp, FaLaravel, FaDatabase, FaBootstrap, FaGitAlt} from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs, SiTypescript, SiUnity} from "react-icons/si";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const experiences = [
+    {
+      title: "Game Programmer at RAION COMMUNITY",
+      date: "Januray 2023 – December 2023",
+      description: "Become a programmer in some game development, such as platformers and educational games.",
+    },
+    {
+      title: "Web Developer at CV. Technobit Indonesia (Intern)",
+      date: "February 2024 – July 2024",
+      description: "Develop and maintain web-based app with Laravel MVC and participate in agile development (stand-up meeting, sprint planning).",
+    },
+    {
+      title: "Web Developer at CV. Technobit Indonesia (Part Time)",
+      date: "August 2024 – December 2024",
+      description: "Develop ERP system for company with Laravel.",
+    },
+    {
+      title: "Graduated from Brawijaya University",
+      date: "August 2025",
+      description: "Bachelor's Degree in Informatics Engineering.",
+    },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 20);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const [text, setText] = useState("");
+  const code =`Hello, World! This is my portfolio page.\n\nMy name is Bagas Aditya Pramudia\nI'm a Full Stack Developer.\n`;
+
+  useEffect(() => {
+    let index = 0;
+    const typing = setInterval(() => {
+      setText(code.slice(0, index));
+      index++;
+      if (index > code.length) clearInterval(typing);
+    }, 50);
+
+    return () => clearInterval(typing);
+  }, []);
+
+  return (
+    <div className="bg-slate-900 font-mono grid items-center justify-items-center min-h-screen gap-10 px-5">
+      <header className={`fixed top-0 left-0 w-full flex items-center justify-center py-8 z-50 transition-colors duration-300 ${
+          scrolled ? "bg-slate/800 backdrop-blur-sm shadow text-gray-300" : "bg-transparent" }`}
+      >
+        <nav className="flex gap-20 text-2xl font-bold">
+          <a href="#about">About</a>
+          <a href="#experience">Experience</a>
+          <a href="#skill">Skill</a>
+          <a href="#project">Project</a>
+        </nav>
+      </header>
+      <main className="flex flex-col w-full min-h-screen justify-center items-center px-20">
+        <div className="flex flex-row w-full justify-center items-center h-[80vh]">
+          {/* <div className="w-[48%] flex flex-col items-center">
+            <div className="gap-5 flex flex-col items-start">
+              <h1 className="text-5xl font-bold">Bagas Aditya Pramudia</h1>
+              <h1 className="text-3xl font-semibold">Full Stack Developer</h1>
+            </div>
+          </div> */}
+          <div className="w-[75%] flex flex-col items-center">
+            <div className="w-fit min-w-2xl max-w-full bg-gray-800 rounded-xl shadow-lg font-mono text-green-400 overflow-auto">
+              <div className="bg-gray-700 px-4 py-3 rounded-t-xl relative">
+                <div className="flex space-x-3">
+                  <span className="w-4 h-4 bg-red-500 rounded-full"></span>
+                  <span className="w-4 h-4 bg-yellow-500 rounded-full"></span>
+                  <span className="w-4 h-4 bg-green-500 rounded-full"></span>
+                </div>
+                <span className="absolute left-1/2 transform -translate-x-1/2 -translate-y-6 text-lg text-white">
+                  ~/portfolio
+                </span>
+              </div>
+              <div className="p-4 text-lg text-start">
+                <pre className="whitespace-pre-wrap">{text}</pre>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row items-center gap-8">
+          <a href="https://github.com/username" target="_blank" rel="noopener noreferrer">
+            <Github className="w-12 h-12 text-white hover:scale-110 transition-transform" />
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+          <a href="https://linkedin.com/in/username" target="_blank" rel="noopener noreferrer">
+            <Linkedin className="w-12 h-12 text-blue-600 hover:scale-110 transition-transform" />
+          </a>
+
+          <a href="https://instagram.com/username" target="_blank" rel="noopener noreferrer">
+            <Instagram className="w-12 h-12 text-pink-500 hover:scale-110 transition-transform" />
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <section id="about" className="max-w-4xl mb-10">
+        <div className="flex flex-col items-center gap-5">
+          <h1 className="text-2xl font-bold">ABOUT ME</h1>
+          <p className="text-lg text-justify">
+            As a fresh graduate from Brawijaya University with a degree in Information Technology, I have gained hands-on experience in web development through academic projects and internships.I specialize in designing and building websites and web applications, with proficiency in front-end technologies such as HTML, CSS, and JavaScript, as well as back-end development using PHP and Laravel. I also have experience with version control systems like Git and database management using MySQL. Driven by a passion for continuous learning, I actively keep up with the latest trends and innovations in web development. With strong problem-solving and communication skills, I work well in collaborative environments and am committed to delivering effective, user-focused solutions.
+          </p>
+        </div>
+      </section>
+      <section id="experience" className="max-w-4xl">
+        <div className="flex flex-col items-center gap-5">
+          <div className="flex items-center justify-center">
+            <h1 className="text-2xl font-bold">MY EXPERIENCE</h1>
+          </div>
+          <div className="relative border-l-5 border-green-600 pl-6 ml-4">
+            {experiences.map((exp, index) => (
+              <div key={index} className="mb-3 relative">
+                <div className="w-4 h-4 bg-green-600 rounded-full absolute -left-2 top-2"></div>
+                <h3 className="text-xl font-bold ml-5 mb-2">{exp.title}</h3>
+                <p className="text-sm text-gray-200">{exp.date}</p>
+                <p className="text-gray-400">{exp.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="skill">
+        <div className="flex flex-col items-center gap-5">
+          <h1 className="text-2xl font-bold">MY SKILLS</h1>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 text-white">
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <FaHtml5 className="text-5xl mx-auto text-orange-600" />
+              <h2 className="text-lg font-semibold mt-2">HTML</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <FaCss3Alt className="text-5xl mx-auto text-blue-500" />
+              <h2 className="text-lg font-semibold mt-2">CSS</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <FaJs className="text-5xl mx-auto text-yellow-400" />
+              <h2 className="text-lg font-semibold mt-2">JavaScript</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <SiTypescript className="text-5xl mx-auto text-blue-400" />
+              <h2 className="text-lg font-semibold mt-2">TypeScript</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <FaDatabase className="text-5xl mx-auto text-teal-400" />
+              <h2 className="text-lg font-semibold mt-2">MySQL</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <FaPhp className="text-5xl mx-auto text-indigo-400" />
+              <h2 className="text-lg font-semibold mt-2">PHP</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <FaLaravel className="text-5xl mx-auto text-red-500" />
+              <h2 className="text-lg font-semibold mt-2">Laravel</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <SiNextdotjs className="text-5xl mx-auto text-white" />
+              <h2 className="text-lg font-semibold mt-2">Next.JS</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <FaBootstrap className="text-5xl mx-auto text-purple-600" />
+              <h2 className="text-lg font-semibold mt-2">Bootstrap</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <SiTailwindcss className="text-5xl mx-auto text-sky-400" />
+              <h2 className="text-lg font-semibold mt-2">Tailwind CSS</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <FaGitAlt className="text-5xl mx-auto text-sky-400" />
+              <h2 className="text-lg font-semibold mt-2">GitHub</h2>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+              <SiUnity className="text-5xl mx-auto text-white" />
+              <h2 className="text-lg font-semibold mt-2">Unity</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="project" className="max-w-4xl">
+        <div className="flex flex-col items-center gap-5">
+          <h1 className="text-2xl font-bold">MY PROJECTS EXPERIENCE</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md">
+              <h2 className="text-xl font-semibold mb-2">Sistem Pemesanan Gedung dan Publikasi</h2>
+              <p className="text-gray-400">A website for managing building bookings and event publication for Goes to Campus event, with integrated VR technology for immersive building visualization.</p>
+              <a href="https://sistempemesanangedung.bagasaditya.com" className="text-blue-400">Go to Project</a>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md">
+              <h2 className="text-xl font-semibold mb-2">SILPUSITRON (Sistem Informasi Layanan Publik Terintegrasi Secara Elektronik) </h2>
+              <p className="text-gray-400">The integrated electronic public service system in Blitar City enables residents to submit certificate requests at the sub-district level and supports end-to-end processing up to the final document printing for official signing.</p>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md">
+              <h2 className="text-xl font-semibold mb-2">Web App Stock Control</h2>
+              <p className="text-gray-400">A web application designed to manage and monitor stock levels in a warehouse, ensuring efficient inventory tracking and item availability.</p>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md">
+              <h2 className="text-xl font-semibold mb-2">$PASHER BEARY</h2>
+              <p className="text-gray-400">A dedicated NFT project website for $PASHER BEARY, featuring digital collectibles, project details, and community links.</p>
+              <a href="https://nft.bagasaditya.com" className="text-blue-400">Go to Project</a>
+            </div>
+            <div className="bg-gray-800 p-6 rounded-lg shadow-md">
+              <h2 className="text-xl font-semibold mb-2">Web Desa Ngadirejo</h2>
+              <p className="text-gray-400">An official profile website that provides public service features for residents, such as submitting requests and accessing village information.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className="flex items-center justify-center w-full max-w-xl py-6 mt-10 bg-gray-800 rounded-lg shadow-md">
+        <p className="text-gray-400 text-sm">
+          &copy; {new Date().getFullYear()} Bagas Aditya Pramudia. All rights reserved.
+        </p>
       </footer>
     </div>
   );
